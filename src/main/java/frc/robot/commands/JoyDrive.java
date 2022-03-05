0cbc4ca4911433fe975522ab4fbd537fa3fd90a2
KshitijTeotia06 <teotia.kshitij@gmail.com>
2022-03-05 12:11:00 -0800

    fixed joydrive stuff

--- src/main/java/frc/robot/commands/JoyDrive.java
+++ src/main/java/frc/robot/commands/JoyDrive.java
@@ -17,16 +17,14 @@ import frc.robot.subsystems.Drivetrain;
 public class JoyDrive extends CommandBase {
   /** Creates a new JoyDrive. */
   private final Drivetrain drivetrain;
-  //private Joystick driveStick, turnStick;
-  private XboxController controller;
+  private Joystick driveStick, turnStick;
   private AHRS ahrsNavX;
 
-  public JoyDrive(Drivetrain dt, XboxController xbox) { //replace parameters w (Drivetrain dt, Joystick dst, Joystick tst) for wheel and stick 
-    drivetrain = dt;      
-    /*driveStick = dst;
-    turnStick = tst; */ 
-    this.controller = xbox;
-     
+  public JoyDrive(Drivetrain dt, Joystick dst, Joystick tst) { //replace parameters w (Drivetrain dt, Joystick dst, Joystick tst) for wheel and stick 
+    this.drivetrain = dt;      
+    this.turnStick = tst; 
+    this.driveStick = dst;
+    
     // Change based on the connection to nav x
   
     ahrsNavX = new AHRS(SerialPort.Port.kUSB);
@@ -42,30 +40,17 @@ public class JoyDrive extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    //driveStick = new Joystick(Constants.DRIVE_STICK_PORT);
-    //turnStick = new Joystick(Constants.TURN_STICK_PORT);
-    controller = new XboxController(Constants.XBOX_PORT);
-
+    driveStick = new Joystick(Constants.DRIVE_STICK_PORT);
+    turnStick = new Joystick(Constants.TURN_STICK_PORT);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-<<<<<<< HEAD
     if(driveStick.getRawButton(0)){
       drivetrain.toggleGear();
     }
     drivetrain.move(-driveStick.getRawAxis(Constants.DRIVE_AXIS), -turnStick.getX());
-=======
-    drivetrain.move(controller.getRawAxis(Constants.DRIVE_AXIS), controller.getRawAxis(Constants.TURN_AXIS)); 
-    /* replace parameters w 
-    driveStick.getRawAxis(Constants.DRIVE_AXIS), turnStick.getRawAxis(Constants.TURN_STICK_PORT) 
-    for final bot with joystick and wheel */
-    
-    SmartDashboard.putNumber("gyroAngleX", ahrsNavX.getAngle());
-    SmartDashboard.updateValues();
-
->>>>>>> d13f6b3d4862082d3b5dc7ba01f755ddcb627aa5
   }
 
   // Called once the command ends or is interrupted.
